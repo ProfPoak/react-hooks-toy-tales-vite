4f8d5b003ea83e74d5f716f509b416104526b638
Porter <pdhakes25@gmail.com>
2026-02-10 08:53:24 -0700

    implemented ToyCard display

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
+  const [toys, setToys] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        else {
+          console.log("Error: could not load toys")
+        }
+      })
+      .then(data => {
+        setToys(data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toys.map(toy => (
+      <ToyCard 
+        key={toy.id}
+        name={toy.name}
+        image={toy.image}
+        likes={toy.likes}
+      />
+    ))}
+    </div>
   );
 }
 
